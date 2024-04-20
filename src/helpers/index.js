2fc7c03e18cd287b71159ba1fb4f8e6555ab2554
oleksandrkravcuk <134079899+oleksandrkravcuk@users.noreply.github.com>
2024-04-20 23:39:41 +0300

    work

---
+++ src/helpers/index.js
@@ -0,0 +1,11 @@
+import returnDetailsItem from "./returnDetailsItem";
+import smoothScrollToTarget from "./smoothScrollToTarget";
+import formatDate from "./formatDate";
+import gerAllSearchParams from "./getAllSearchParams";
+
+export {
+  returnDetailsItem,
+  smoothScrollToTarget,
+  formatDate,
+  gerAllSearchParams,
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store";
+import { ThemeProvider } from "contexts";
+import App from "components/App/App.jsx";
+import "modern-normalize";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
